fcfa0b3e0632090c4662a3319e16b9c341f92d9c
mhmmdcills <manangmmaruf80@gmail.com>
2019-12-03 16:10:01 +0700

    JS-WebDesign

---
+++ javascript/belajar_js5.js
@@ -0,0 +1,25 @@
+//var pembanding1 = 5;
+//var pembanding2 = "5" 
+var tag_hasil = document.getElementById('hasil');
+if ((1>0 && 2<4 )|| 4<5) {
+	tag_hasil.innerHTML = "true";
+}
+else {
+	tag_hasil.innerHTML = "false";
+}
+
+//var hasil = (2<4) ? "iyo":"ora";
+//tag_hasil.innerHTML;
+
+//var pembanding1 = 5;
+//var pembanding2 = "5" 
+//var tag_hasil = document.getElementById('hasil');
+//if (pembanding1 === pembanding2) {
+	//tag_hasil.innerHTML = "true";}
+//else {
+	//tag_hasil.innerHTML = "false";}
+//==, ===, !=, !==, >, <, >=, <=
+
+//pembanding= (||)(|O)(O|)(OO)
+//&&        = (|)  (O) (|) (O)
+//||        = (|) (|)  (|) (O)
---
+++ tugasarray.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pelajaran Array</title>
+</head>
+	<body>		
+		<?php
+		//1.PHP Array adalah :  merupakan tipe data yang menyimpan satu data, bahkan lebih didalam variabel tunggal yang sesuai dengan fungsinya, bisa dibilang tipe data yang unik.
+
+		//2A. Array Indexed : array yang menyimpan setiapbagian array dengan indeks numerik
+		//2B. Index Asosiatif : array yang indeksnya ditentukan secara manual, bisa menggunakan angka atau simbol tertentu
+		
+		//3. Contoh array indexed
+			//cara1
+		$motor = array("Scoopy", "Nmax", "Vario");
+			//cara2
+		$motor[0] = "Scoopy";
+		$motor[1] = "Nmax";
+		$motor[2] = "Vario";
+		?>
+	</body>
+</html>
